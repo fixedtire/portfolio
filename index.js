2cd1cb271beb1135a75aafee9f08478acbf224af
bencake <hagstrom_b@gmx.de>
2023-07-02 14:33:58 +0200

    slide-in animation working correctly now

---
+++ index.js
@@ -0,0 +1,19 @@
+//setting up inbuilt JavaScript function IntersectionObserver
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-class");
+    }
+
+    /* makes Animation goes on/off depending on observation
+    else entry.target.classList.remove("slide-class"); */
+  });
+});
+
+// Loops over every <section>-Element with class "toSlide"
+const objectsToSlide = document.querySelectorAll(".toSlide");
+objectsToSlide.forEach(function (element) {
+  //observes every element in this array
+  observer.observe(element);
+});
